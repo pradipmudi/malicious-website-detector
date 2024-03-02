de44eba410e35e7fab4708a5f12bdb07df90c437
Pradip Mudi <pradipmudi24@gmail.com>
2024-03-02 22:26:23 +0530

    Initial commit

---
+++ src/main/java/com/maliciouswebsitedetector/MaliciousWebsiteDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.maliciouswebsitedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaliciousWebsiteDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaliciousWebsiteDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maliciouswebsitedetector/MaliciousWebsiteDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliciouswebsitedetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaliciousWebsiteDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
